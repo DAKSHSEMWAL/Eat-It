2793eaf7e45dc77750160ac9493a945da1d00575
DAKSH SEMWAL <dsemwal2@gmail.com>
2019-08-14 11:41:32 +0530

    "Added Android X Support"

---
+++ ServerSide/app/src/main/java/com/kuro/daksh/eaititserver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kuro.daksh.eaititserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
